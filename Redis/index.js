ddfc721646f0182e36666efbebd12a5375e988a5
prince kumar <agarwal8789@gmail.com>
2024-08-25 11:31:12 +0530

    commit made on Redis

--- Redis/index.js
+++ Redis/index.js
@@ -1,48 +1,32 @@
 const net = require('net');
-const Parser = require('redis-parser');
+const { handleRequest } = require('./requestHandler');
 const express = require('express');
 const app = express();
-const store = {};
+const PORT = 6030;
+
 const server = net.createServer(connection => {
-    console.log('client connected');
-    connection.on('data', data => {
-        const parser = new Parser({
-            returnReply: function (reply) {
-                const command = reply[0];
-                switch (command) {
-                    case 'set': {
-                        const key = reply[1];
-                        const value = reply[2];
-                        store[key] = value;
-                        connection.write('OK\r\n');
-                    }
-                        break;
-                    case 'get': {
-                        const key = reply[1];
-                        const value = store[key];
-                        if (!value) connection.write('$-1\r\n');
-                        else connection.write(`$${value.length}\r\n${value}\r\n`);
-                    }
-                        break;
-                }
-            },
-            returnError: function (err) {
-                console.log('error:', err);
-            },
-            returnFatalError: function (err) {
-                console.log('fatal error:', err);
-            },
-            returnBuffers: true
-        });
-        parser.execute(data);
-    })
-})
-server.listen(6030, () =>
-    console.log('server is running on port:6030')
-)
+    console.log('client connected...');
+
+    connection.on('data', async data => {
+        // console.log('Received data:', data.toString());
+
+        try {
+            await handleRequest(data, connection);
+        } catch (error) {
+            // console.error('Error:', error);
+            connection.write(`-${error}\r\n`);
+        }
+    });
+
+    connection.on('end', () => {
+        console.log('client disconnected...');
+    });
+});
+
+server.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 app.get('/', (req, res) => {
     res.send('Hello Redis');
 })
 app.listen(3000, () => {
-    console.log('server is running on port:http://localhost:3000');
+    console.log('Server started on port https://localhost:3000');
 })
---
+++ Redis/respParser.js
@@ -0,0 +1,74 @@
+const NULL_BULK_STRING = "NULL";
+const READ_CACHE_MAX = 8192; // 8 KB
+const ITEM_LEN_MAX = 536870912; // 512 MB
+
+class RESPParser {
+    constructor(buffer) {
+        this.buffer = buffer;
+        this.offset = 0;
+        this.readCache = '';
+    }
+
+    async updateCache() {
+        const remaining = this.buffer.length - this.offset;
+        const chunkSize = Math.min(READ_CACHE_MAX, remaining);
+        this.readCache = this.buffer.slice(this.offset, this.offset + chunkSize).toString();
+        this.offset += chunkSize;
+    }
+
+    async cacheHasValidItem() {
+        for (let i = 0; i < this.readCache.length; i++) {
+            if (this.readCache[i] === '\r' && this.readCache[i + 1] === '\n') {
+                const item = this.readCache.slice(0, i + 2);
+                this.readCache = this.readCache.slice(i + 2);
+                return item;
+            }
+        }
+        return null;
+    }
+
+    async readNextItem() {
+        let item = '';
+
+        while (true) {
+            const validItem = await this.cacheHasValidItem();
+            if (validItem) {
+                item += validItem;
+                break;
+            }
+            await this.updateCache();
+        }
+
+        return item;
+    }
+
+    async readNewRequest() {
+        const arrSizeItem = await this.readNextItem();
+        const size = parseInt(arrSizeItem.slice(1, -2));
+
+        const req = [];
+
+        for (let i = 0; i < size; i++) {
+            const bulkStrSizeItem = await this.readNextItem();
+            const bulkStrSize = parseInt(bulkStrSizeItem.slice(1, -2));
+
+            if (bulkStrSize === -1) {
+                req.push(NULL_BULK_STRING);
+                continue;
+            }
+
+            const bulkStrItem = await this.readNextItem();
+            const bulkStr = bulkStrItem.slice(0, -2);
+
+            if (bulkStr.length !== bulkStrSize) {
+                throw new Error("Bulk string size does not match");
+            }
+
+            req.push(bulkStr);
+        }
+
+        return req;
+    }
+}
+
+module.exports = RESPParser;
